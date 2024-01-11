3d27a4063a86cc5265e67a96c985b066e2a34821
marielean <elena01.mari@edu.unife.it>
2024-01-11 17:23:38 +0100

    added comment

--- analogchess.py
+++ analogchess.py
@@ -17,7 +17,9 @@ pygame.init()
 
 from chessboardex import *
 
-# inizia il gioco con il turno del bianco
+# Possible heuristics: custom_heuristic_0, custom_heuristic_1
+# Possible algorithms: MiniMax, AlphaBeta, Random
+
 ia = IA(utility=custom_heuristic_0, algorithm = 'AlphaBeta', depth = 2)
 # board = Board(granularity=10, pieces=chessboard_5) # Use this if you want to test a specific chessboard
 board = Board(granularity=1)
