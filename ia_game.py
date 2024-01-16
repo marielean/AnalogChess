6501ff24ae673eb937db7e01c3b0d84a0f0efb99
marielean <elena01.mari@edu.unife.it>
2024-01-16 15:03:04 +0100

    tolti commenti inutili

--- ia_game.py
+++ ia_game.py
@@ -1,4 +1,4 @@
-# import pygame, time, random
+
 from differentfiles.pieces import *
 from differentfiles.ia import IA
 from differentfiles.colors import *
@@ -14,8 +14,6 @@ from differentfiles.drawing import (
 from differentfiles.heuristics import custom_heuristic_0, custom_heuristic_1, custom_heuristic_2
 
 
-
-# print("pygame is running:", pygame.get_init()) 
 
 
 
@@ -64,8 +62,7 @@ turn_number = 1
 
 just_played = False
 white_turn = True
-# pygame.display.quit()
-# print("pygame is running:", pygame.display.get_init())
+
 print(f"Gioco iniziato con white algorithm: {white_ia_algorithm} e depth: {white_depth} con euristica: {white_heuristic.__name__}")
 print(f"Gioco iniziato con black algorithm: {black_ia_algorithm} e depth: {black_depth} con euristica: {black_heuristic.__name__}")
 print(f"Granularità: {granularity}")
@@ -128,11 +125,7 @@ while not done:
             print(f"Euristica del perdente usato: {white_heuristic.__name__ if board.is_white_turn() else black_heuristic.__name__}")
             print(f"Granularità: {granularity}")
             done = True
-            # if pygame.display.get_init():
-            #     font = pygame.font.SysFont("oldenglishtext", int(80))
-            #     confirm_text = font.render("Wiiiiiiin", True, black)
-            #     draw_center_text(confirm_text)
-            #     pygame.display.flip()
+            
 
     
 
