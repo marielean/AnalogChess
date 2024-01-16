d8b0daafe16ad3ff742c30c41a6a0c00bc501384
marielean <elena01.mari@edu.unife.it>
2024-01-16 17:58:36 +0100

    file di testing per partite tra ia

--- ia_game.py
+++ ia_game.py
@@ -1,9 +1,20 @@
+import pygame, time, random
 from differentfiles.pieces import *
 from differentfiles.ia import IA
 from differentfiles.colors import *
+from differentfiles.drawing import (
+    see_through,
+    see_through2,
+    draw_line_round_corners_polygon,
+    draw_checkers,
+    screen,
+    draw_center_text,
+)
 from differentfiles.heuristics import custom_heuristic_0, custom_heuristic_1, custom_heuristic_2
 
-# from chessboardex import *
+pygame.init()
+
+from chessboardex import *
 
 # Possible heuristics: custom_heuristic_0, custom_heuristic_1, custom_heuristic_2
 # Possible algorithms: MiniMax, AlphaBeta, Random
@@ -14,17 +25,38 @@ black_ia = IA(utility=custom_heuristic_0, algorithm = 'Random', depth = 2)
 board = Board(granularity=1)
 
 done = False
+clock = pygame.time.Clock()
+# confirmed = True
 
 turn_number = 0
 
+pygame.display.set_caption("Analog Chess")
+
+draw_line_round_corners_polygon(
+    see_through, (120, 120), (220, 220), RED_HIGHLIGHT, 0.7 * 640 / 8
+)
+
 for piece in board.get_pieces():
     piece.calc_paths(board.get_pieces())
-          
 
 while not done:
 
     for piece in board.get_pieces():
         piece.calc_paths(board.get_pieces())
+
+    draw_checkers()
+    screen.blit(see_through, (0, 0))
+    screen.blit(see_through2, (0, 0))
+
+    for piece in board.get_pieces():
+        piece.draw()
+
+    pygame.display.flip()
+    clock.tick(30)
+
+    see_through.fill((0, 0, 0, 0))
+
+    see_through2.fill((0, 0, 0, 0))
 
     #White starts playing
     if board.is_white_turn():
@@ -43,20 +75,34 @@ while not done:
                 done = True
                 break
 
+    draw_checkers()
+    screen.blit(see_through, (0, 0))
+    screen.blit(see_through2, (0, 0))
+
+    for piece in board.get_pieces():
+        piece.draw()
+
+    pygame.display.flip()
+    clock.tick(30)
+
+    see_through.fill((0, 0, 0, 0))
+
+    see_through2.fill((0, 0, 0, 0))
+
     #Black plays
     if not board.is_white_turn():
 
         if not board.is_terminal():
             best_move = black_ia.get_best_move(board)
             board.apply_move(best_move)
-
             print("Turno numero: ", turn_number)
             print("Mossa selezionata dal giocatore NERO: ", best_move)
             print("Pezzo giocato dal NERO", best_move[0])
-        
-        turn_number += 1
-        board.set_turn(True)
+
+            turn_number += 1
+            board.set_turn(True)
 
         if board.is_terminal():
             print("This is the end! Black wins!")
             done = True
+            pygame.display.flip()
