89763c85e65951288f9089e996306d7fd428e8d6
marielean <elena01.mari@edu.unife.it>
2024-01-30 14:56:32 +0100

    aggiunto lo stallo

--- withoutgraphic.py
+++ withoutgraphic.py
@@ -32,7 +32,11 @@ def run_match(utility_white, utility_black, algorithm_white, algorithm_black, de
             end_time = time.time()
             print('#', sep='', end='', flush=True)
             return board.is_white_turn(), end_time - start_time, turn_number
-
+        if turn_number >= 2000:
+            end_time = time.time()
+            print('#', sep='', end='', flush=True)
+            return None, end_time - start_time, turn_number
+        
         board.set_turn(not board.is_white_turn())
         turn_number += 1
 
